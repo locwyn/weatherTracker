f65153d26b82578242fe53e43486bcdfaaaac2ea
locwyn <36432201+locwyn@users.noreply.github.com>
2018-06-12 20:18:01 -0500

    Update hourlyTemps.py
    changing to average temps in a day for comparison

--- hourlyTemps.py
+++ hourlyTemps.py
@@ -8,17 +8,23 @@ def readFileIntoList(fileName):
     return recList
         
 def parseData(testData):
+  numRecs = 0
+  sumOfTemps = 0
   for i in testData:
     splitRec = i.split(",")
-    print(datetime.datetime.fromtimestamp(float(splitRec[0]) - 18000).strftime('%c') + " : " + splitRec[3])
+    #print(datetime.datetime.fromtimestamp(float(splitRec[0]) - 18000).strftime('%c') + " : " + splitRec[3])
     #print(datetime.datetime.fromtimestamp(float(splitRec[0])).strftime('%H%M') + ":" + splitRec[1])
-    
-    
-#fileName = "2017_06_16_weather_obs_riv.txt"
-#filePath = "/home/gbk/data/weatherTracker/oldOWMData/"
-fileName = "seedStarterTemps20170616.txt"
-filePath = "/home/gbk/data/weatherTracker/seedStarter/"
+    numRecs += 1
+    sumOfTemps += float(splitRec[1])
+  averageTempForDay = sumOfTemps / numRecs
+  return averageTempForDay
+ 
+
+fileName = "2017_06_16_weather_obs_riv.txt"
+filePath = "/home/gbk/data/weatherTracker/oldOWMData/"
+#fileName = "seedStarterTemps20170616.txt"
+#filePath = "/home/gbk/data/weatherTracker/seedStarter/"
 fullFileName = filePath + fileName
 
 testData = readFileIntoList(fullFileName)
-parseData(testData)
+print(parseData(testData))
