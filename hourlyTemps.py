680f4d910503ec45bbdf6f69a9c1fbfb96b1ee4b
locwyn <36432201+locwyn@users.noreply.github.com>
2018-06-12 20:35:21 -0500

    Update hourlyTemps.py
    testing the average function in the looping function

--- hourlyTemps.py
+++ hourlyTemps.py
@@ -28,7 +28,7 @@ def loopDateRange():
     #fileName = "seedStarterTemps20170616.txt"
     #filePath = "/home/gbk/data/weatherTracker/seedStarter/"
     fullFileName = filePath + fileName
-    print(fileName)
+    print(processRegDate + " - " + parseData(fullFileName))
 
 #testData = readFileIntoList(fullFileName)
 #print(parseData(testData))
